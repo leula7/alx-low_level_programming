6c998a57cc12b3f4b5e6a906a077e12283e57dfc
leula7 <leulkahssaye100@gmail.com>
2022-04-07 08:32:28 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,10 @@
  * File: 4-puts.c
  * Auth: Brennan D Baraban <375@holbertonschool.com>
  */
+
 #include <stdio.h>
-	   /**
+
+/**
  * main - Prints "Programming is like building a multilingual
  *                puzzle, followed by a new line.
  *
@@ -12,5 +14,6 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
