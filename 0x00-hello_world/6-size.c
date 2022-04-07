3dd6934e4823d6e48505a7ad9b774b8eb7f37dcc
leula7 <leulkahssaye100@gmail.com>
2022-04-07 08:40:48 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+/*
+ * File: 4-puts.c
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints size of various types on the computer
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%d,%d,%d",sizeof(int),sizeof(long),sizeof(char));
+
+	return (0);
+}
