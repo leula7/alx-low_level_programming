752bbd981caf298889ad5656d1558f0e2a54837d
leula7 <leulkahssaye100@gmail.com>
2022-04-07 09:16:30 -0700

    advance task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+/*
+ * File: 6-size.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
