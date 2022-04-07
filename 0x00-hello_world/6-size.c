52c7c475f8f8610117e698e3bbf95bed2e70b906
leula7 <leulkahssaye100@gmail.com>
2022-04-07 09:06:12 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,24 @@
 /*
- * File: 4-puts.c
- * Auth: Brennan D Baraban <375@holbertonschool.com>
+ * File: 6-size.c
+ * Auth: Brennan D Baraban
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints size of various types on the computer
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize 
-	of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize 
-	of a float: %d byte(s)\n",sizeof(char),sizeof(int),sizeof(long),
-	sizeof(long long),sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
-        return (0);
+	return (0);
 }
 
