cf0b9c5d9d689fc1ce513c75c731fc3c79d72d1e
leula7 <leulkahssaye100@gmail.com>
2022-04-12 07:48:47 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,8 +15,10 @@ void print_alphabet_x10(void)
 	for(i=1; i<=10; i++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+		{
+			 _putchar(letter);
+		}
 		_putchar('\n');
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
