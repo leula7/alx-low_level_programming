a72093653be22ce3038c6024ed8cb5cb7d041736
leula7 <leulkahssaye100@gmail.com>
2022-04-12 08:31:08 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,13 +14,13 @@
  */
 int print_sign(int n)
 {
-	int n;
+	int c;
 
-	if (n>0) 
+	if (c>0) 
 		_putchar('+');
 		return (1);
 	else
-        	if (n>0)
+        	if (c>0)
                 	_putchar('0');
 		return (0);
 	else
