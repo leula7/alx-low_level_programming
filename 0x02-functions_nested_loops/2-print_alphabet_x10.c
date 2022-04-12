093e9badf133584d6fa02ea35ad5a52837937fc8
leula7 <leulkahssaye100@gmail.com>
2022-04-12 07:45:07 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,8 @@
 void print_alphabet_x10(void)
 {
 	char letter;
-	for(int i=1; i<=10; i++)
+	int i=1;
+	for(i=1; i<=10; i++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
