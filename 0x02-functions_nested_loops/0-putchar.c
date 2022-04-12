2b9dcc9b460ec99a06673d4fa3902c3823d9030e
leula7 <leulkahssaye100@gmail.com>
2022-04-12 06:34:25 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,14 @@
 
  int _putchar(void)
 {
-    _putchar('_putchar');
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _purtchar('h');
+    _putchar('a');
+    _putchr('r');
     _putchar('\n');
 	return (0);
 
