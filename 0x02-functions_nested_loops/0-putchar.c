70f660ab74566972ea911cb92e3f5cfb84b98e8c
leula7 <leulkahssaye100@gmail.com>
2022-04-12 06:46:23 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,19 @@
-/*
- * File: 0_putchar.c
- * 
- *
- */
-
 #include "main.h"
 
 /**
- * main - Prints "Holberton" followed by a new line.
- *
- * Return: Always 0.
+ * main - Entry point
+ * Return:0
  */
+
 int main(void)
 {
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+char c[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
+}
 _putchar('\n');
+return (0);
 }
