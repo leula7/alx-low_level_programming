bcc4192f2a8c846078e2228902a3136230de4b7f
leula7 <leulkahssaye100@gmail.com>
2022-04-12 08:29:42 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * Auth: L K
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _isalpha - Checks if a character is alphabetic.
@@ -12,7 +12,7 @@
  *
  * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
-int _isalpha(int c)
+int print_sign(int n)
 {
 	int n;
 
