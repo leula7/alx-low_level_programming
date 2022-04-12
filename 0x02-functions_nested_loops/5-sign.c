e3dec653517ffa44e455d710d8f2c34801efc5c2
leula7 <leulkahssaye100@gmail.com>
2022-04-12 08:27:37 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+
+/*
+ * File: 5-sign.c
+ * Auth: L K
+ */
+
+#include "holberton.h"
+
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	int n;
+
+	if (n>0) 
+		_putchar('+');
+		return (1);
+	else
+        	if (n>0)
+                	_putchar('0');
+		return (0);
+	else
+		_putchar('-');
+		return (-1);
+}
