2e5cd58846647b61b15effe30c02a73c02e250b9
leula7 <leulkahssaye100@gmail.com>
2022-04-12 07:39:14 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,19 @@
-/**
- * file 1-alphabet.c
- */
-#include "main.h"
-void print_alphabet(void)
-{
-	/**
- * print_alphabet - prints the alphabet in lowercase, 
- * followed by a new line.
+/*
+ * File: 1-alphabet.c
+ * Auth: L K
  */
 
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
+
 	_putchar('\n');
 }
