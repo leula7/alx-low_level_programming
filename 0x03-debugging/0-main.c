123ba735c15d145a43c3349e34b13a44cd18814c
leula7 <leulkahssaye100@gmail.com>
2022-04-13 10:19:23 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -14,33 +14,3 @@ positive_or_negative(i);
 
 return (0);
 }
-
-
-Task 1 "1-main.c"
-
-Ans:
-
-#include <stdio.h>
-
-/**
-* main - causes an infinite loop
-* Return: 0
-*/
-
-int main(void)
-{
-int i;
-
-printf("Infinite loop incoming :(\n");
-
-i = 0;
-/*
-*while (i < 10)
-*{
-*		putchar(i);
-*}
-*/
-printf("Infinite loop avoided! \\o/\n");
-
-return (0);
-}
