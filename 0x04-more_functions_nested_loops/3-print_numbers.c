517ff06aa1f58cfbabac43640103f086e2a59a12
leula7 <leulkahssaye100@gmail.com>
2022-04-14 08:33:55 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+#include "main.h"
+void print_numbers(void)
+{
+	int i;
+	for(i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
