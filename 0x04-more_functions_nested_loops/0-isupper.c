641f1a80e012be0ea210fd56a175657e3724b435
leula7 <leulkahssaye100@gmail.com>
2022-04-14 07:16:15 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(c)
+{
+	int i = c;
+	if(i >=65 || i <= 90)
+	{
+		return (1);
+	}
+	else if(i >= 97 || i <= 122);
+	{
+		return (0);
+	}
+}
