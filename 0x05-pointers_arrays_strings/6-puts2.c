98cf27b56ed9a5deff671c5ee5502281733eb2b7
leula7 <75134713+leula7@users.noreply.github.com>
2022-04-18 23:23:14 +0300

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,12 +12,13 @@
 void puts2(char *str)
 {
 	int index = 0, len = 0;
-
-	while (str[index++])
-		len++;
-
-	for (index = 0; index < len; index += 2)
-		_putchar(str[index]);
-
-	_putchar('\n');
+{
+while (str[index++])
+len++;	
+}
+{
+for (index = 0; index < len; index += 2)
+_putchar(str[index]);
+}
+_putchar('\n');
 }
