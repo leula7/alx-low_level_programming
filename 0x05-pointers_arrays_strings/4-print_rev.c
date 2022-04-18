94287bbdfafbdfe1e01666b8cdb61ac289aab86e
leula7 <75134713+leula7@users.noreply.github.com>
2022-04-18 23:53:32 +0300

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,8 +4,6 @@
  */
 
 #include "main.h"
-#include <string.h>
-#include <stdio.h>
 
 /**
  * print_rev - Prints a string in reverse.
@@ -13,6 +11,14 @@
  */
 void print_rev(char *s)
 {
-strrev(s);
-puts(s);	
+int len = 0, index;
+{
+while (s[index++])
+len++;
+}
+{
+for (index = len - 1; index >= 0; index--)
+  _putchar(s[index]);
+}
+_putchar('\n');
 }
