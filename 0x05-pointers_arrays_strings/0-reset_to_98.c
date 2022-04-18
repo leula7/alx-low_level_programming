02501a87461b6199bcb10cbb4ca7ee16a9206e2a
leula7 <75134713+leula7@users.noreply.github.com>
2022-04-18 22:18:26 +0300

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-/**
- * main - check the code 
- *
- * Return: Always 0.
+/*
+ * Write a function that takes a pointer to an int as 
+ * parameter and updates the value it points to to 98
+ * @n: size of the n
  */
 void reset_to_98(int *m)
 {
