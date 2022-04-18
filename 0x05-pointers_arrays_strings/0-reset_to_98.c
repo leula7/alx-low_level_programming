4806e6ea1d69b31687dfe7ce8194ef744b9fa16d
leula7 <75134713+leula7@users.noreply.github.com>
2022-04-18 20:12:25 +0300

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,15 @@
  * File: 0-reset_to_98.c
  * Auth: Leul K
  */
-#include "main.h"
+
+#include "holberton.h"
+
 /**
- * main - check the code 
+* main - check the code 
  *
  * Return: Always 0.
  */
 void reset_to_98(int *m)
 {
-*m = 98;
+	*m = 98;
 }
