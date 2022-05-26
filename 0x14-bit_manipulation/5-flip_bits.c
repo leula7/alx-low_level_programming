c115e3af7fe63e48c457e3df0d3f4887634623d5
leula7 <75134713+leula7@users.noreply.github.com>
2022-05-26 20:15:47 +0300

    Update 5-flip_bits.c
    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,9 +1,9 @@
 /*
  * File: 5-flip_bits.c
- * Auth: Brennan D Baraban
+ * Auth: Leul K
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
  * flip_bits - Counts the number of bits needed to be
