563819f1cd84bbb8c77e8767b9e194bc024ecd45
leula7 <75134713+leula7@users.noreply.github.com>
2022-05-26 20:14:40 +0300

    Update 2-get_bit.c
    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,9 @@
 /*
  * File: 2-get_bit.c
- * Auth: Brennan D Baraban
+ * Auth: Leul K
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
  * get_bit - Gets the value of a bit at a given index.
