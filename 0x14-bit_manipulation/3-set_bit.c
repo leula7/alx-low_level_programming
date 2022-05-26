ba4264bef2e29a338ed0fc1258ffd897fd6fec46
leula7 <75134713+leula7@users.noreply.github.com>
2022-05-26 20:15:03 +0300

    Update 3-set_bit.c
    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,9 +1,9 @@
 /*
  * File: 3-set_bit.c
- * Auth: Brennan D Baraban
+ * Auth: Leul K
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
  * set_bit - Sets the value of a bit at a given index to 1.
